64d48a5f0a78aeda7b80350453750dd8701d3dd9
Nathan Lubchenco <nathan@simpleenergy.com>
2014-12-30 13:33:22 -0700

    publishing deck

--- make_slides.R
+++ make_slides.R
@@ -1,6 +1,6 @@
 library('slidify')
 
-#author("nathan_lubchenco")
-slidify("index.Rmd")
+#author("data_literacy")
+slidify("data_literacy/index.Rmd")
 publish(user = "nathanlubchenco", repo = "presentations")
 
